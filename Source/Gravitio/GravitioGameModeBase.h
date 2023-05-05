8713d8d1eeebc52cc458834afce94e3f05b49afd
Aexl <axellavignepro@gmail.com>
2023-05-05 23:42:14 +0200

    Initial commit

---
+++ Source/Gravitio/GravitioGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GravitioGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRAVITIO_API AGravitioGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
